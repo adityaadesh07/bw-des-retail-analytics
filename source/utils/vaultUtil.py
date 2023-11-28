c40d5a13f37821ea816f9fbbed538d123b47f8b9
adityaadesh07 <adityaadeshpande07@gmail.com>
2023-11-28 17:19:38 +0530

    Added vaultUtils

---
+++ source/utils/vaultUtil.py
@@ -0,0 +1,50 @@
+import requests
+
+vault_url = "http://127.0.0.1:8200"
+role_id = "4665b9ec-6d2c-41be-e5dc-2ce62d10c934"
+secret_id = "1ae817be-b471-d46c-e4a7-d93faf94a67f"
+secret_path = "secret/data/aws"
+
+def authenticate_with_approle():
+    auth_url = f"{vault_url}/v1/auth/approle/login"
+    auth_data = {
+        "role_id": role_id,
+        "secret_id": secret_id
+    }
+    try:
+        auth_response = requests.post(auth_url, json=auth_data)
+        auth_response.raise_for_status()
+
+        token = auth_response.json()["auth"]["client_token"]
+        print()
+        print("Token===========",)
+        return token
+    
+    except requests.exceptions.RequestException as e:
+        print(f"Authentication error : {e}")
+        return None
+
+def get_secret(token):
+    
+    headers = {
+        "X-Vault-Token": token,
+    }
+    url = f"{vault_url}/v1/{secret_path}"
+
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+
+        secret_data = response.json()["data"]
+
+        return secret_data
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error retrieving secret: {e}")  
+        return None  
+
+
+token = authenticate_with_approle()
+secret_data = get_secret(token)
+
+print(secret_data)
